552d624ceea72f087123b1af20d4ae580ac3baf7
codinglil <102619991+codinglil@users.noreply.github.com>
2022-12-07 19:03:16 -0500

    updated html, added js with random messages

---
+++ script.js
@@ -0,0 +1,27 @@
+const btn = document.getElementById("button");
+const toasts = document.getElementById("toasts");
+const messages = [
+  "Message One",
+  "Message Two",
+  "Message Three",
+  "Message Four",
+  "Message Five",
+];
+
+btn.addEventListener("click", () => createNotification());
+
+function createNotification() {
+  const notification = document.createElement("div");
+  notification.classList.add("toast");
+  notification.innerText = getRandomMessage();
+
+  toasts.appendChild(notification);
+
+  setTimeout(() => {
+    notification.remove();
+  }, 3000);
+}
+
+function getRandomMessage() {
+  return messages[Math.floor(Math.random() * messages.length)];
+}
